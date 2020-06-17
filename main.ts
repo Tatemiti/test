ce669c8b34191dd4300234c496ad52cb7e805691
Tatemiti <67031981+Tatemiti@users.noreply.github.com>
2020-06-17 16:05:16 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,30 @@
-basic.showIcon(IconNames.Heart)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (true) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+    basic.showIcon(IconNames.Target)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(2)
+    basic.showIcon(IconNames.Square)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+    basic.showIcon(IconNames.Scissors)
+})
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-	
+    radio.setGroup(167)
 })
