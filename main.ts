ad8ad0cdaffd6c6735722dd700ca14c4529e0a1c
Tatemiti <67031981+Tatemiti@users.noreply.github.com>
2020-06-24 11:58:47 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,40 @@
 radio.onReceivedNumber(function (receivedNumber) {
+    basic.clearScreen()
+    basic.pause(1000)
     if (true) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else {
-    	
+        basic.showIcon(IconNames.Triangle)
+    } else if (receivedNumber == 0 && (receivedNumber == 1 || receivedNumber == 1 && (receivedNumber == 2 || receivedNumber == 2 && receivedNumber == 0))) {
+        basic.showIcon(IconNames.No)
+    } else if (receivedNumber == 0 && receivedNumber == 2 || receivedNumber == 1 && (receivedNumber == 0 || receivedNumber == 2 && receivedNumber == 1)) {
+        basic.showIcon(IconNames.SmallHeart)
     }
 })
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.pause(500)
     radio.sendNumber(0)
     basic.showIcon(IconNames.Target)
+    receivedNumber = 0
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Heart)
+    receivedNumber = 3
 })
 input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    basic.pause(500)
     radio.sendNumber(2)
     basic.showIcon(IconNames.Square)
+    receivedNumber = 2
 })
 input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.pause(500)
     radio.sendNumber(1)
     basic.showIcon(IconNames.Scissors)
+    receivedNumber = 1
 })
+let receivedNumber = 0
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
     radio.setGroup(167)
